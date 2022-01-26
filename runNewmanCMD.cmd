d3cf2ce2df8a1f174c69698bed35c8060d679704
Qin Lu <qlu@lightboxre.com>
2022-01-25 17:04:11 -0800

    Added command to run 'My Star War API'.

--- runNewmanCMD.cmd
+++ runNewmanCMD.cmd
@@ -1,1 +1,1 @@
-newman run Collections/"My Okta API - Users.postman_collection.json" -e Envs/"Okta Test.postman_environment.json"
+newman run Collections/"My Okta API - Users.postman_collection.json" -e Envs/"Okta Test.postman_environment.json" & newman run Collections/"My Star War API.postman_collection.json" 
